1d250ae4b8d013f51b198206a0a3fe8f271d70bc
Sara Herman <saraherman92@gmail.com>
2013-10-09 15:38:57 -0700

    Alpha Release 0.2
    Added header.php, footer.php, and index.php.
    Fixed issues related to slider positioning.
    Index page complete.

---
+++ footer.php
@@ -0,0 +1,10 @@
+<div class="footer">
+		<div class="container">
+			<div>
+				<p class="left">Copyright 2010 AVALANE INC. All rights reserved.</p>
+				<p class="right">Design by gerbengeeraerts for MOJO-THEMES.COM</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index</title>
+	<script src="http://code.jquery.com/jquery-1.6.3.min.js"></script>
+	<script src="script.js"></script>
+	<link rel="stylesheet" type="text/css" href="normalize.css">
+	<link rel="stylesheet" type="text/css" href="main.css">	
+</head>
+<body>
+	<div class="block-top">
+		<div class="header">
+			<div class="container">
+				<div class="logo left">
+					<h1>Avalane<br><span class="indent small">DIGITAL CONCEPT ART</span></h1>
+				</div>
+
+				<div class="nav-bar">
+					<ul class="right">
+						<li><a href="">home</a><br><span class="light-text">Start here</span></li>
+						<li><a href="">about</a><br><span class="light-text">Learn more about us</span></li>
+						<li><a href="">portfolio</a><br><span class="light-text">See our previous work</span></li>
+						<li><a href="">blog</a><br><span class="light-text">News and stuff</span></li>
+						<li><a href="">contact</a><br><span class="light-text">Contact us</span></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
